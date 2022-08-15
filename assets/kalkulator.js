2796a0514a3023f1e9c4c42f42a2f126530131d1
mertaseptiawan <mertaseptiawan@gmail.com>
2022-08-15 13:24:59 +0800

    menambahkan fungsi tombol clear pada file kalkulator.js

--- assets/kalkulator.js
+++ assets/kalkulator.js
@@ -1,33 +1,42 @@
 const calculator = {
-    displayNumber: "0",
+    displayNumber: '0',
     operator: null,
     firstNumber: null,
     isWaitForSecondNumber: false,
-}
-
-function updateDisplay() {
+  };
+   
+  function updateDisplay() {
     document.querySelector('#displayNumber').innerText = calculator.displayNumber;
-}
-
-function clearCalculatoor() {
-    calculator.displayNumber = "0";
+  }
+   
+  function clearCalculator() {
+    calculator.displayNumber = '0';
     calculator.operator = null;
     calculator.firstNumber = null;
     calculator.isWaitForSecondNumber = false;
-}
-
-function inputDigit(digit) {
-    calculator.displayNumber += digit;
-}
-
-const buttons = document.querySelectorAll('button');
-
-for (const button of buttons) {
-    button.addEventListener('click', function (event) {
-        //mendapatkan object elemen yang diklik
-        const target = event.target;
-
-        inputDigit(terget.innerText);
-        updateDisplay();
+  }
+   
+  function inputDigit(digit) {
+    if (calculator.displayNumber === '0') {
+      calculator.displayNumber = digit;
+    } else {
+      calculator.displayNumber += digit;
+    }
+  }
+   
+  const buttons = document.querySelectorAll('.button');
+  for (const button of buttons) {
+    button.addEventListener('click', function(event) {
+      // mendapatkan objek elemen yang diklik
+      const target = event.target;
+   
+      if (target.classList.contains('clear')) {
+          clearCalculator();
+          updateDisplay();
+          return;
+      }
+   
+      inputDigit(target.innerText);
+      updateDisplay();
     });
-}
+  }
